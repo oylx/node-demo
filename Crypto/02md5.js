4b8e48fb747ad881948ae884674917e7c79548bc
oylx <oylx2008@163.com>
2019-11-08 11:24:19 +0800

    change function name

--- Crypto/02md5.js
+++ Crypto/02md5.js
@@ -25,14 +25,14 @@
  * 计算所有传入数据的 hash 摘要。参数 encoding（编码方式）可以为 hex、binary、base64。
  */
 const crypto = require('crypto');
-const 02 = str => {
+const md5 = str => {
     return crypto.createHash('md5').update(str, 'utf8').digest('hex')
 };
 
 // 默认输出长度为32位小写字母
 // 25f9e794323b453885f5181f1b624d0b
-console.log(02('123456789'));
+console.log(md5('123456789'));
 
 // 以下转换为32位大写字母
 // 25F9E794323B453885F5181F1B624D0B
-console.log(02('123456789').toUpperCase());
+console.log(md5('123456789').toUpperCase());
